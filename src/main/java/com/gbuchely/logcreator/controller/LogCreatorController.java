1402d46b4d83d1a7f471be16c39344c3fc4fa3bf
Daniel Buitrago <Daniel.Buitrago@wnco.com>
2019-01-04 15:42:38 -0500

    [Daniel Buitrago] Implemented log4j2

--- src/main/java/com/gbuchely/logcreator/controller/LogCreatorController.java
+++ src/main/java/com/gbuchely/logcreator/controller/LogCreatorController.java
@@ -1,23 +1,28 @@
 package com.gbuchely.logcreator.controller;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-import java.util.logging.Logger;
+
 
 @RestController
 public class LogCreatorController {
 
-    private static final Logger LOGGER = Logger.getLogger(LogCreatorController.class.getName());
+    private static final Logger LOGGER = LogManager.getLogger(LogCreatorController.class.getName());
 
     private String testMessage = "This is a test message";
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     private String start() {
-        LOGGER.info(testMessage);
+        LOGGER.info("This is a INFO message");
+        LOGGER.debug("This is a DEBUG message");
+        LOGGER.error("This is a ERROR message");
+        LOGGER.warn("This is a WARN message");
         return testMessage;
     }
 
